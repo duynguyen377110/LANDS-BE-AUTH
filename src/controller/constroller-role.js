809c16ea0f94d78e444e7c636d3de726237a5d7c
duy366110 <duy366110@gmail.com>
2024-04-01 22:59:35 +0700

    #Config done microservice get all and get role by id.

--- src/controller/constroller-role.js
+++ src/controller/constroller-role.js
@@ -10,6 +10,45 @@ class ControllerRole {
     constructor() { }
 
     /**
+     * GET ALL ROLE
+     */
+    async getAllRole() {
+        try {
+            const CONNECT = getCloud();
+            const ROLECONSUMER = configQueue.AUTH.ALL_ROLE.CONSUMMER_ALL_ROLE;
+            const ROLEREFLY = configQueue.AUTH.ALL_ROLE.REFLY_ALL_ROLE;
+
+            AmqpConsumer.consumer(CONNECT, ROLECONSUMER, async (information) => {
+                let roles = await ServiceRole.getAllRole();
+                AmqpProducer.producer(CONNECT, ROLEREFLY, JSON.stringify({roles}));
+            })
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    /**
+     * GET ROLE BY ID
+     */
+    async getRoleById() {
+        try {
+            const CONNECT = getCloud();
+            const ROLECONSUMER = configQueue.AUTH.GET_ROLE_BY_ID.CONSUMMER_GET_ROLE_BY_ID;
+            const ROLEREFLY = configQueue.AUTH.GET_ROLE_BY_ID.REFLY_GET_ROLE_BY_ID;
+
+            AmqpConsumer.consumer(CONNECT, ROLECONSUMER, async (information) => {
+                let { id } = information;
+                let role = await ServiceRole.getRoleById(id);
+                AmqpProducer.producer(CONNECT, ROLEREFLY, JSON.stringify({role}));
+            })
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    /**
      * CREATE ROLE
      */
     async createRole() {
