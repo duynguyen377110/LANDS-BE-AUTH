fd5c9ed7f8e538f49a3cd92aec75abde31b93c77
duy366110 <duy366110@gmail.com>
2024-04-03 17:53:39 +0700

    #Config server not sleep.

--- src/index.js
+++ src/index.js
@@ -7,23 +7,27 @@ const ControllerUser = require("./controller/controller-user");
 const app = express();
 
 amqpCore(async () => {
-    // AUTH
-    await ControllerAuth.authSignin();
-    await ControllerAuth.authSignout();
-    
-    // ROLE
-    await ControllerRole.getAllRole();
-    await ControllerRole.getRoleById();
-    await ControllerRole.createRole();
-    await ControllerRole.updateRole();
-    await ControllerRole.deleteRole();
+    app.use("/", async (req, res, next) => {
+        // AUTH
+        await ControllerAuth.authSignin();
+        await ControllerAuth.authSignout();
+        
+        // ROLE
+        await ControllerRole.getAllRole();
+        await ControllerRole.getRoleById();
+        await ControllerRole.createRole();
+        await ControllerRole.updateRole();
+        await ControllerRole.deleteRole();
 
-    // USER
-    await ControllerUser.getAll();
-    await ControllerUser.getUserById();
-    await ControllerUser.createUser();
-    await ControllerUser.updateUser();
-    await ControllerUser.deleteUser();
+        // USER
+        await ControllerUser.getAll();
+        await ControllerUser.getUserById();
+        await ControllerUser.createUser();
+        await ControllerUser.updateUser();
+        await ControllerUser.deleteUser();
+
+        return res.status(200).json({status: true, message: "auth"});
+    })
 })
 
 module.exports = app;
